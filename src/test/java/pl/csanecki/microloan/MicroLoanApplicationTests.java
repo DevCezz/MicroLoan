3e7df17c7590c9871b69d9b0acb0728b75dddc54
Cezary <c.sanecki@gmail.com>
2020-01-21 18:59:54 +0100

    Initial commit

---
+++ src/test/java/pl/csanecki/microloan/MicroLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.csanecki.microloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroLoanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
