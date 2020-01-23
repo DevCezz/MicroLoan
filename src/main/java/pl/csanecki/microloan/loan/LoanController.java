154bee6b162292e10cd9e3a3c694ca60bee3db2a
Cezary Sanecki <csanecki@gmail.com>
2020-01-23 13:07:21 +0100

    created acceptance test for loan controller

--- src/main/java/pl/csanecki/microloan/loan/LoanController.java
+++ src/main/java/pl/csanecki/microloan/loan/LoanController.java
@@ -1,15 +1,24 @@
 package pl.csanecki.microloan.loan;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 import pl.csanecki.microloan.loan.dto.LoanQuery;
 import pl.csanecki.microloan.loan.dto.UserRequest;
 import pl.csanecki.microloan.loan.model.Disposition;
+import pl.csanecki.microloan.loan.model.PositiveDisposition;
 import pl.csanecki.microloan.loan.service.LoanService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
 
 @RestController
 public class LoanController {
@@ -21,10 +30,38 @@ public class LoanController {
         this.loanService = loanService;
     }
 
-    @PostMapping("/loan/query")
-    Disposition queryForLoan(@RequestBody LoanQuery loanQuery, HttpServletRequest request) {
+    @PostMapping(
+        value = "/loan/query",
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<Disposition> queryForLoan(@RequestBody LoanQuery loanQuery, HttpServletRequest request) {
         UserRequest userRequest = UserRequest.extractFrom(request);
+        Disposition disposition = loanService.considerLoanRequest(userRequest, loanQuery);
 
-        return loanService.considerLoanRequest(userRequest, loanQuery);
+        return responseConsidering(disposition);
+    }
+
+    private ResponseEntity<Disposition> responseConsidering(Disposition disposition) {
+        if(isPositiveDisposition(disposition)) {
+            return ResponseEntity
+                    .created(resolveLocationUri((PositiveDisposition) disposition))
+                    .body(disposition);
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_ACCEPTABLE)
+                    .body(disposition);
+        }
+    }
+
+    private URI resolveLocationUri(PositiveDisposition disposition) {
+        return UriComponentsBuilder
+                .fromUriString("/loan/postpone/{id}")
+                .buildAndExpand(disposition.getLoanId())
+                .toUri();
+    }
+
+    private boolean isPositiveDisposition(Disposition disposition) {
+        return disposition instanceof PositiveDisposition;
     }
 }
--- src/test/java/pl/csanecki/microloan/loan/LoanControllerTest.java
+++ src/test/java/pl/csanecki/microloan/loan/LoanControllerTest.java
@@ -1,17 +1,13 @@
 package pl.csanecki.microloan.loan;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import pl.csanecki.microloan.loan.dto.LoanQuery;
 import pl.csanecki.microloan.loan.dto.UserRequest;
 import pl.csanecki.microloan.loan.model.Disposition;
@@ -19,55 +15,44 @@ import pl.csanecki.microloan.loan.model.LoanStatus;
 import pl.csanecki.microloan.loan.model.PositiveDisposition;
 import pl.csanecki.microloan.loan.service.LoanService;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
-@AutoConfigureMockMvc
+@WebMvcTest(LoanController.class)
 class LoanControllerTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private LoanService loanService;
-
-    private MockHttpServletRequest mockRequest;
-
-    private UserRequest mockedUserRequest;
-
-    @BeforeEach
-    void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(new LoanController(loanService)).build();
-
-        mockRequest = new MockHttpServletRequest();
-        mockedUserRequest = UserRequest.extractFrom(mockRequest);
-
-        ReflectionTestUtils.setField(mockedUserRequest, "ip", "10.0.0.90");
-        ReflectionTestUtils.setField(mockedUserRequest, "requestTimestamp", LocalDateTime.of(2020, 1, 20, 13, 26));
-    }
 
     @Test
     void shouldGrantLoan() throws Exception {
         //given
         Disposition disposition = new PositiveDisposition("Pożyczka została pomyślnie wydana", LoanStatus.GRANTED, 27L);
-        LoanQuery loanQuery = new LoanQuery(BigDecimal.valueOf(10000), 36);
 
         //when
-        when(loanService.considerLoanRequest(mockedUserRequest, loanQuery)).thenReturn(disposition);
+        when(loanService.considerLoanRequest(any(UserRequest.class), any(LoanQuery.class))).thenReturn(disposition);
 
         //then
         mockMvc
                 .perform(post("/loan/query")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{ \"amount\": 10000, \"periodInMonths\": 36")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
+                        .content("{ \"amount\":10000, \"periodInMonths\":36 }")
+                        .characterEncoding("UTF-8")
+                        .accept(MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML))
+                .andDo(print())
+                .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(header().string("Location", "/loan/27"))
-                .andExpect(jsonPath("$.loadId").value(27));
+                .andExpect(content().json("{ \"message\":\"Pożyczka została pomyślnie wydana\", \"loanStatus\":\"GRANTED\", \"loanId\":27 }"))
+                .andExpect(header().string("Location", "/loan/postpone/27"))
+                .andExpect(jsonPath("$.message").value("Pożyczka została pomyślnie wydana"))
+                .andExpect(jsonPath("$.loanStatus").value("GRANTED"))
+                .andExpect(jsonPath("$.loanId").value(27));
     }
 }
